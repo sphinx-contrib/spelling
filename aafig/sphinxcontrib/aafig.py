79cc11b856bd4509a806670b5284e99affd2e8a1
Leandro Lucarella <llucax@gmail.com>
2009-07-02 12:04:05 -0300

    aafig: Make Aafig directive inherit from the Image
    This is a major rewrite of the extension. The Aafig directive creates an image
    node with some extra information to allow a future processing to render the
    aafigure image.
    
    The Sphinx event 'doctree-read' is used to do the image rendering and update
    the image URI with the proper value, based on the current builder being use.
    
    As a side effect, now aafig directive accepts all the image directive too
    (width, height, alt, align, target and class) and SVG format is properly
    handled in HTML builders.
    * * *
    amend

--- aafig/sphinxcontrib/aafig.py
+++ aafig/sphinxcontrib/aafig.py
@@ -29,17 +29,29 @@ from sphinx.util.compat import Directive
 
 import aafigure
 
-DEFAULT_PREFIX = 'aafig'
+
+def merge_defaults(options, config):
+    # merge default options
+    for (k, v) in config.aafig_default_options.items():
+        if k not in options:
+            options[k] = v
+    return options
+
+
+def get_basename(text, options, prefix='aafig'):
+    options = options.copy()
+    if 'format' in options:
+        del options['format']
+    hashkey = text.encode('utf-8') + str(options)
+    id = sha(hashkey).hexdigest()
+    return '%s-%s' % (prefix, id)
+
 
 class AafigError(SphinxError):
     category = 'aafig error'
 
 
-class aafig(nodes.General, nodes.Element):
-    pass
-
-
-class Aafig(Directive):
+class AafigDirective(directives.images.Image):
     """
     Directive to insert an ASCII art figure to be rendered by aafigure.
     """
@@ -47,8 +59,7 @@ class Aafig(Directive):
     required_arguments = 0
     optional_arguments = 0
     final_argument_whitespace = False
-    option_spec = dict(
-        scale        = float,
+    own_option_spec = dict(
         line_width   = float,
         background   = str,
         foreground   = str,
@@ -57,37 +68,76 @@ class Aafig(Directive):
         textual      = directives.flag,
         proportional = directives.flag,
     )
+    option_spec = directives.images.Image.option_spec.copy()
+    option_spec.update(own_option_spec)
 
     def run(self):
-        node = aafig()
-        node['text'] = '\n'.join(self.content)
-	if 'textual' in self.options: self.options['textual'] = True
-	if 'proportional' in self.options: self.options['proportional'] = True
-        node['options'] = self.options
-        return [node]
+        aafig_options = dict()
+        image_attrs = dict()
+        own_options_keys = self.own_option_spec.keys() + ['scale']
+        for (k, v) in self.options.items():
+            if k in own_options_keys:
+                # convert flags to booleans
+                if v is None:
+                    v = True
+                # convert percentage to float
+                if k == 'scale':
+                    v = float(v) / 100
+                aafig_options[k] = v
+                del self.options[k]
+        self.arguments = ['']
+        (image_node,) = directives.images.Image.run(self)
+        if isinstance(image_node, nodes.system_message):
+            return [image_node]
+        text = '\n'.join(self.content)
+	image_node.aafig = dict(options = aafig_options, text = text)
+        return [image_node]
 
 
-def render_aafigure(self, text, options, prefix):
+def render_aafig_images(app, doctree):
+    for img in doctree.traverse(nodes.image):
+        if not hasattr(img, 'aafig'):
+            continue
+
+        options = img.aafig['options']
+        text = img.aafig['text']
+        format = app.builder.format
+        merge_defaults(options, app.builder.config)
+        try:
+            try:
+                options['format'] = app.builder.config.aafig_format[format]
+            except:
+                app.builder.warn('unsupported builder format "%s", please '
+                        'add a custom entry in aafig_format config option '
+                        'for this builder' % format)
+                continue
+            fname, outfn, id, extra = render_aafigure(app, text, options)
+        except AafigError, exc:
+            app.builder.warn('aafigure error: ' + str(exc))
+            current_node.replace_self([])
+            # TODO: replace with the ascii art itself
+        img['uri'] = fname
+        # FIXME: find some way to avoid this hack in aafigure
+        if extra:
+            (width, height) = [x.split('"')[1] for x in extra.split()]
+            (img['width'], img['height']) = (width, height)
+
+
+def render_aafigure(app, text, options):
     """
     Render an ASCII art figure into the requested format output file.
     """
 
-    # merge default options
-    for (k, v) in self.builder.config.aafig_default_options.items():
-        if k not in options:
-            options[k] = v
-
-    hashkey = text.encode('utf-8') + str(options)
-    id = sha(hashkey).hexdigest()
-    fname = '%s-%s.%s' % (prefix, id, options['format'])
-    if hasattr(self.builder, 'imgpath'):
+    fname = get_basename(text, options)
+    fname = '%s.%s' % (get_basename(text, options), options['format'])
+    if hasattr(app.builder, 'imgpath'):
         # HTML
-        relfn = posixpath.join(self.builder.imgpath, fname)
-        outfn = path.join(self.builder.outdir, '_images', fname)
+        relfn = posixpath.join(app.builder.imgpath, fname)
+        outfn = path.join(app.builder.outdir, '_images', fname)
     else:
         # LaTeX
         relfn = fname
-        outfn = path.join(self.builder.outdir, fname)
+        outfn = path.join(app.builder.outdir, fname)
     metadata_fname = '%s.aafig' % outfn
 
     try:
@@ -126,53 +176,9 @@ def render_aafigure(self, text, options, prefix):
     return relfn, outfn, id, extra
 
 
-def render_html(self, node, text, options, prefix=DEFAULT_PREFIX, imgcls=None):
-    try:
-        options['format'] = self.builder.config.aafig_format['html']
-        fname, outfn, id, extra = render_aafigure(self, text, options, prefix)
-    except AafigError, exc:
-        self.builder.warn('aafigure error: ' + str(exc))
-        raise nodes.SkipNode
-
-    self.body.append(self.starttag(node, 'p', CLASS='aafigure'))
-    if fname is None:
-        self.body.append(self.encode(text))
-    else:
-        imgcss = imgcls and 'class="%s"' % imgcls or ''
-        if options['format'].lower() == 'svg':
-            self.body.append('<object type="image/svg+xml" data="%s" %s %s />'
-                    % (fname, extra, imgcss))
-        else:
-            self.body.append('<img src="%s" alt="%s" %s/>\n' %
-                    (fname, self.encode(text).strip(), imgcss))
-    self.body.append('</p>\n')
-    raise nodes.SkipNode
-
-
-def html_visit(self, node):
-    render_html(self, node, node['text'], node['options'])
-
-
-def render_latex(self, node, text, options, prefix=DEFAULT_PREFIX):
-    try:
-        options['format'] = self.builder.config.aafig_format['latex']
-        fname, outfn, id, extra = render_aafigure(self, text, options, prefix)
-    except AafigError, exc:
-        self.builder.warn('aafigure error: ' + str(exc))
-        raise nodes.SkipNode
-
-    if fname is not None:
-        self.body.append('\\includegraphics[]{%s}' % fname)
-    raise nodes.SkipNode
-
-
-def latex_visit(self, node):
-    render_latex(self, node, node['text'], node['options'])
-
-
 def setup(app):
-    app.add_node(aafig, html=(html_visit, None), latex=(latex_visit, None))
-    app.add_directive('aafig', Aafig)
+    app.add_directive('aafig', AafigDirective)
+    app.connect('doctree-read', render_aafig_images)
     app.add_config_value('aafig_format', dict(html='svg', latex='pdf'), 'html')
     app.add_config_value('aafig_default_options', dict(), 'html')
 
