8055f9ae667dd42c760122096dbc5f44bca0393b
Dan MacKinlay <dan@possumpalace.org>
2009-12-02 15:20:54 +1100

    corrected date handling tests

--- feed/tests/test_build.py
+++ feed/tests/test_build.py
@@ -42,23 +42,26 @@ FEED_WARNINGS = ENV_WARNINGS + ""
 
 def exists(p):
     assert os.path.exists(p)
-    
+def eq(a, b):
+    assert a==b
+
 @gen_with_app(buildername='html', warning=feed_warnfile, cleanenv=True)
 def test_feed(app):
     app.build(all_files=True, filenames=[]) #build_all misses the crucial finish signal
     feed_warnings = feed_warnfile.getvalue().replace(os.sep, '/')
     feed_warnings_exp = FEED_WARNINGS % {'root': app.srcdir}
-    yield ops.eq, feed_warnings, feed_warnings_exp
+    yield eq, feed_warnings, feed_warnings_exp
     rss_path = os.path.join(app.outdir, 'rss.xml')
     yield exists, rss_path
-    yield ops.eq, app.builder.env.feed_items.keys(), ['older', 'most_aged', 'latest']
+    yield eq, app.builder.env.feed_items.keys(), ['older', 'most_aged', 'latest']
     # see http://www.feedparser.org/
     f = feedparser.parse(rss_path)
+    yield eq, f.bozo, 0 #feedparser well-formedness detection
     entries = f.entries
-    yield ops.eq, entries[0].updated_parsed, (2001, 8, 11)
-    yield ops.eq, entries[0].title, "The latest blog post"
-    yield ops.eq, entries[1].updated_parsed, (2001, 8, 1)
-    yield ops.eq, entries[1].title, "An older blog post"
-    yield ops.eq, entries[2].updated_parsed, (1979, 1, 1)
-    yield ops.eq, entries[2].title, "The oldest blog post"
+    yield eq, entries[0].updated_parsed[0:3], (2001, 8, 11)
+    yield eq, entries[0].title, "The latest blog post"
+    yield eq, entries[1].updated_parsed[0:3], (2001, 8, 1)
+    yield eq, entries[1].title, "An older blog post"
+    yield eq, entries[2].updated_parsed[0:3], (1979, 1, 1)
+    yield eq, entries[2].title, "The oldest blog post"
 
