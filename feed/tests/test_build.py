9b7f6b77cf417b3e050680f9b6ba064ba35fd618
Dan MacKinlay <dan@possumpalace.org>
2010-01-24 17:32:06 +0800

    tests fixed to create empty dirs so as to work on fresh checkout

--- feed/tests/test_build.py
+++ feed/tests/test_build.py
@@ -1,13 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-    test_build
-    ~~~~~~~~~~
-
-    Test the entire build process with the test root.
-
-    :copyright: Copyright 2007-2009 by the Sphinx team, see AUTHORS.
-    :license: BSD, see LICENSE for details.
-"""
 
 import os
 from os.path import exists
@@ -22,45 +13,41 @@ from util import test_root, raises, raises_msg, Struct,\
 
 import feedparser
 
-import nose.tools
+from nose.tools import assert_equals
+
+def setup_module():
+    if not (test_root / '_static').exists():
+        (test_root / '_static').mkdir() #this empty dir is not included in version control
 
 def teardown_module():
     (test_root / '_build').rmtree(True)
 
 feed_warnfile = StringIO()
 
-ENV_WARNINGS = ""
+ENV_WARNINGS = "" #none at the moment. if needed, they will look like:
 # """\
 # %(root)s/includes.txt:4: WARNING: download file not readable: nonexisting.png
 # """
 
-FEED_WARNINGS = ENV_WARNINGS + ""
-# """\
-# %(root)s/images.txt:20: WARNING: no matching candidate for image URI u'foo.*'
-# """
-
-def exists(p):
-    assert os.path.exists(p)
-def eq(a, b):
-    assert a==b
+FEED_WARNINGS = ENV_WARNINGS + "" # nothing here either yet
 
 @gen_with_app(buildername='html', warning=feed_warnfile, cleanenv=True)
 def test_feed(app):
     app.build(all_files=True, filenames=[]) #build_all misses the crucial finish signal
     feed_warnings = feed_warnfile.getvalue().replace(os.sep, '/')
     feed_warnings_exp = FEED_WARNINGS % {'root': app.srcdir}
-    yield eq, feed_warnings, feed_warnings_exp
+    yield assert_equals, feed_warnings, feed_warnings_exp
     rss_path = os.path.join(app.outdir, 'rss.xml')
     yield exists, rss_path
-    yield eq, app.builder.env.feed_items.keys(), ['older', 'most_aged', 'latest']
+    yield assert_equals, app.builder.env.feed_items.keys(), ['older', 'most_aged', 'latest']
     # see http://www.feedparser.org/
     f = feedparser.parse(rss_path)
-    yield eq, f.bozo, 0 #feedparser well-formedness detection
+    yield assert_equals, f.bozo, 0 #feedparser well-formedness detection
     entries = f.entries
-    yield eq, entries[0].updated_parsed[0:3], (2001, 8, 11)
-    yield eq, entries[0].title, "The latest blog post"
-    yield eq, entries[1].updated_parsed[0:3], (2001, 8, 1)
-    yield eq, entries[1].title, "An older blog post"
-    yield eq, entries[2].updated_parsed[0:3], (1979, 1, 1)
-    yield eq, entries[2].title, "The oldest blog post"
+    yield assert_equals, entries[0].updated_parsed[0:3], (2001, 8, 11)
+    yield assert_equals, entries[0].title, "The latest blog post"
+    yield assert_equals, entries[1].updated_parsed[0:3], (2001, 8, 1)
+    yield assert_equals, entries[1].title, "An older blog post"
+    yield assert_equals, entries[2].updated_parsed[0:3], (1979, 1, 1)
+    yield assert_equals, entries[2].title, "The oldest blog post"
 
