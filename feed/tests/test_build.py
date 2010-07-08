5eeada4a60ca33fff1fd27644b630d643e9debd2
Dan MacKinlay <dan@possumpalace.org>
2010-07-08 18:56:10 +0800

    new tests to capture nastiness around preserving RSS across sessions

--- feed/tests/test_build.py
+++ feed/tests/test_build.py
@@ -31,9 +31,9 @@ ENV_WARNINGS = "" #none at the moment. if needed, they will look like:
 
 FEED_WARNINGS = ENV_WARNINGS + "" # nothing here either yet
 
-@gen_with_app(buildername='html', warning=feed_warnfile, cleanenv=True)
-def test_feed(app):
-    app.build(all_files=True, filenames=[]) #build_all misses the crucial finish signal
+def test_feed():
+    app = TestApp(buildername='html', warning=feed_warnfile, cleanenv=True)  
+    app.build(force_all=True, filenames=[]) #build_all misses the crucial finish signal
     feed_warnings = feed_warnfile.getvalue().replace(os.sep, '/')
     feed_warnings_exp = FEED_WARNINGS % {'root': app.srcdir}
     yield assert_equals, feed_warnings, feed_warnings_exp
@@ -50,5 +50,17 @@ def test_feed(app):
     yield assert_equals, entries[1].title, "An older blog post"
     yield assert_equals, entries[2].updated_parsed[0:3], (1979, 1, 1)
     yield assert_equals, entries[2].title, "The oldest blog post"
-    
-
+    #Now we do it all again to make sure that things work when handling stale files
+    app2 = TestApp(buildername='html', warning=feed_warnfile)  
+    app2.build(force_all=False, filenames=['most_aged'])
+    f = feedparser.parse(rss_path)
+    yield assert_equals, f.bozo, 0 #feedparser well-formedness detection. We want this.
+    entries = f.entries
+    yield assert_equals, entries[0].updated_parsed[0:3], (2001, 8, 11)
+    yield assert_equals, entries[0].title, "The latest blog post"
+    yield assert_equals, entries[1].updated_parsed[0:3], (2001, 8, 1)
+    yield assert_equals, entries[1].title, "An older blog post"
+    yield assert_equals, entries[2].updated_parsed[0:3], (1979, 1, 1)
+    yield assert_equals, entries[2].title, "The oldest blog post"
+    app.cleanup()
+    app2.cleanup()
