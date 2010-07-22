3a71c1f3a5236faab866a037c1af1e8f2c49a729
Matt Williams <matt@milliams.com>
2010-07-22 10:59:39 +0100

    Prep for PyPi submission.

--- doxylink/setup.py
+++ doxylink/setup.py
@@ -2,16 +2,13 @@
 
 from setuptools import setup, find_packages
 
-long_desc = '''
-This package contains the doxylink Sphinx extension.
-
-.. add description here ..
-'''
+with open('README.rst') as stream:
+    long_desc = stream.read()
 
 requires = ['Sphinx>=0.6']
 
 setup(
-    name='doxylink',
+    name='sphinxcontrib-doxylink',
     version='0.1',
     url='http://bitbucket.org/birkenfeld/sphinx-contrib',
     download_url='http://pypi.python.org/pypi/doxylink',
