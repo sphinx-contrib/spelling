c0291902f60865f9d2b993b847d2fe64d662b597
Sebastian Wiesner <lunaryorn@googlemail.com>
2010-07-27 00:45:16 +0200

    Converted intersphinx mapping to sphinx 1.0 format and removed broken sphinx 1.0 inventory

--- epydoc/doc/conf.py
+++ epydoc/doc/conf.py
@@ -19,8 +19,9 @@ exclude_patterns = ['_build']
 html_theme = 'default'
 html_static_path = []
 
-intersphinx_mapping = {'http://docs.python.org/': None,
-                       'http://sphinx.pocoo.org/': None}
+intersphinx_mapping = {'python': ('http://docs.python.org/', None),}
+                       #broken in sphinx 1.0
+                       #'sphinx': ('http://sphinx.pocoo.org/', None)}
 
 
 def setup(app):
