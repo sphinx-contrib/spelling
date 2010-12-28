1166e310e239081d62e09f11f017141314414cc1
Doug Hellmann <doug.hellmann@gmail.com>
2010-12-28 09:53:13 -0500

    start the spelling checker builder

---
+++ spelling/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README', 'r') as f:
+    long_desc = f.read()
+
+requires = ['Sphinx>=0.6',
+            #'PyEnchant>=1.6.5',
+            ]
+
+setup(
+    name='sphinxcontrib-spelling',
+    version='0.1',
+    url='http://bitbucket.org/birkenfeld/sphinx-contrib',
+    download_url='http://pypi.python.org/pypi/sphinxcontrib-spelling',
+    license='BSD',
+    author='Doug Hellmann',
+    author_email='doug.hellmann@gmail.com',
+    description='Sphinx "spelling" extension',
+    long_description=long_desc,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+)
---
+++ spelling/sphinxcontrib/spelling.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
+#
+"""Spelling checker extension for Sphinx.
+"""
+
+from docutils.nodes import GenericNodeVisitor
+#from docutils import core
+from docutils.writers import Writer
+from sphinx.builders import Builder
+from sphinx.util.console import bold, darkgreen
+from docutils.frontend import OptionParser
+from sphinx.util.nodes import inline_all_toctrees
+from docutils.io import StringOutput
+
+import itertools
+import re
+import textwrap
+import imp
+
+import enchant
+from enchant.tokenize import get_tokenizer
+
+# FIXME - Directive (or comment syntax?) to allow words to be ignored in a document
+# FIXME - Words with multiple uppercase letters treated as classes and ignored
+# FIXME - Show counts for misspelled words but not every occurrence
+
+class SpellingChecker(object):
+    """Checks the spelling of blocks of text.
+
+    Uses options defined in the sphinx configuration file to control
+    the checking and filtering behavior.
+    """
+    
+    def __init__(self, lang, suggest):
+        # FIXME - personal word list file
+        self.dictionary = enchant.Dict(lang)
+        # FIXME - filters
+        self.tokenizer = get_tokenizer(lang)
+        self.suggest = suggest
+        
+    _class_name_pattern = re.compile(r'[A-Z]+[a-z0-9]+[A-Z]+[a-z0-9]+')
+    _spelling_word_pattern = re.compile(r'\w+', re.UNICODE|re.MULTILINE|re.DOTALL)
+    def check(self, text):
+        """Generator function that yields bad words and suggested alternate spellings.
+        """
+        for word, pos in self.tokenizer(text):
+            correct = self.dictionary.check(word)
+            if correct:
+                continue
+
+            # FIXME - filter to ignore acronyms
+#             if word.upper() == word:
+#                 # assume acronyms are right
+#                 continue
+#             if word.rstrip('s').upper() == word.rstrip('s'):
+#                 # plural acronyms, too
+#                 continue
+
+            # FIXME - filter to ignore python builtins
+#             if word in __builtins__:
+#                 # python built-in
+#                 continue
+
+            # FIXME - filter to ignore "code-like" patterns, maybe via an option defining regex?
+#             if '_' in word:
+#                 # probably something from code
+#                 continue
+
+            # FIXME - option to ignore importable modules
+#             try:
+#                 imp.find_module(word)
+#             except ImportError:
+#                 pass
+#             else:
+#                 # importable module name
+#                 try:
+#                     SPELLER.addtoSession(word)
+#                 except aspell.AspellSpellerError:
+#                     pass
+#                 continue
+
+            # FIXME - option to include suggestions
+            yield word, self.dictionary.suggest(word) if self.suggest else []
+
+        return
+
+class Section(object):
+    
+    def __init__(self, spelling_checker):
+        self.title = ''
+        self.spelling_checker = spelling_checker
+        self.errors = []
+        self.subsections = []
+        
+    def get_text_lines(self):
+        lines = self.errors[:]
+
+        for subsection in self.subsections:
+            lines.extend(' ' + line
+                         for line in subsection.get_text_lines()
+                         )
+
+        if lines:
+            lines.insert(0, self.title)
+            lines.insert(0, '')
+        return lines
+    
+    def add_subsection(self, subsection):
+        self.subsections.append(subsection)
+        return
+    
+    def set_title(self, title):
+        self.title = title
+        self.check_spelling(title)
+        
+    def astext(self):
+        lines = itertools.chain(*tuple(section.get_text_lines()
+                                       for section in self.subsections
+                                       )
+                                 )
+        # May want this formatting when suggestions are added
+        real_lines = []
+        for line in lines:
+            initial_indent = line[:len(line)-len(line.lstrip())]
+            if line.lstrip().startswith('-'): # error line
+                subsequent_indent = initial_indent + '  '
+            else:
+                subsequent_indent = initial_indent
+            line = textwrap.fill(line.lstrip(), width=70,
+                                 initial_indent=initial_indent,
+                                 subsequent_indent=subsequent_indent,
+                                 )
+            real_lines.append(line)
+        return '\n'.join(real_lines)
+
+    def add_text(self, text):
+        self.check_spelling(text)
+    
+    def check_spelling(self, text):
+        for word, suggestions in self.spelling_checker.check(text):
+            msg = '- "%s"' % word
+            if suggestions:
+                msg = '%s: %s' % (msg, ', '.join(suggestions))
+            self.errors.append(msg)
+        return
+        
+TEXT_NODES = set([ 'block_quote', 'paragraph',
+                   'list_item', 'term', 'definition_list_item', ])
+    
+
+class SpellingVisitor(GenericNodeVisitor):
+    """A Visitor class; see the docutils for more details.
+    """
+    def __init__(self, *args, **kw):
+        GenericNodeVisitor.__init__(self, *args, **kw)
+        # FIXME - Set the language from a configuration option
+        # FIXME - Combine dictionary with personal word list
+        self.spelling_checker = SpellingChecker('en_US', False)
+        self.sections = [ Section(self.spelling_checker) ]
+
+    def visit_title(self, node):
+        # Set the title for the current section.
+        if not self.sections[-1].title:
+            self.sections[-1].set_title(node.astext())
+
+    def visit_section(self, node):
+        # Start a new section
+        new_section = Section(self.spelling_checker)
+        self.sections[-1].add_subsection(new_section)
+        self.sections.append(new_section)
+        
+    def depart_section(self, node):
+        # Pop the section stack
+        self.sections.pop()
+
+    def visit_Text(self, node):
+        if node.parent.tagname in TEXT_NODES:
+            self.sections[-1].add_text(node.astext())
+
+    def default_visit(self, node): pass
+    def default_departure(self, node): pass
+
+    # Ignore inline literal text
+    def visit_literal(self, node): pass
+    def depart_literal(self, node): pass
+    def visit_emphasis(self, node): pass
+    def depart_emphasis(self, node): pass
+
+    # Ignore comment blocks
+    def visit_comment(self, node): pass
+    def depart_comment(self, node): pass
+
+    # Ignore literal blocks
+    def visit_literal_block(self, node): pass
+
+    def astext(self):
+        body = '\n'.join(section.astext()
+                         for section in self.sections
+                         )
+        return body
+
+
+class SpellingWriter(Writer):
+    """Boilerplate attaching our Visitor to a docutils document."""
+    def translate(self):
+        visitor = SpellingVisitor(self.document)
+        self.document.walkabout(visitor)
+        self.output = '\n' + visitor.astext() + '\n'
+
+
+class SpellingBuilder(Builder):
+    """
+    Spell checks a document
+    """
+    name = 'spelling'
+
+    def init(self):
+        self.docnames = []
+        self.document_data = []
+
+    def get_outdated_docs(self):
+        return 'all documents'
+
+    def write(self, *ignored):
+        self.output = []
+        
+        master = self.config.master_doc
+        docwriter = SpellingWriter()
+        docsettings = OptionParser(
+            defaults=self.env.settings,
+            components=(docwriter,)).get_default_values()
+            
+        tree = self.env.get_doctree(master)
+        tree = inline_all_toctrees(self, set(), master, tree, darkgreen)
+        tree['docname'] = master
+
+        destination = StringOutput(encoding='utf-8')
+        text = docwriter.write(tree, destination).strip()
+        if text:
+            self.output.append(text)
+
+        return
+    
+    def finish(self):
+        # FIXME - Use color output?
+        print
+        print
+        for text in self.output:
+            print text
+            print
+        self.info('done')
+
+def setup(app):
+    print 'Initializing Spelling Checker'
+    app.add_builder(SpellingBuilder)
+    return
