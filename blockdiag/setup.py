d70393e852ef57726ec95e8dc57f23fdf76ef3bb
tk0miya <i.tkomiya@gmail.com>
2011-03-10 16:45:27 +0900

    * Update version to blockdiag-0.7.1

--- blockdiag/setup.py
+++ blockdiag/setup.py
@@ -26,7 +26,7 @@ requires = ['blockdiag>=0.6.7', 'Sphinx>=0.6']
 
 setup(
     name='sphinxcontrib-blockdiag',
-    version='0.7.0',
+    version='0.7.1',
     url='http://bitbucket.org/birkenfeld/sphinx-contrib',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-blockdiag',
     license='BSD',
