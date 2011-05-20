b2c7319ddb98cc60f967cbdbc8431bd56d9ef768
tk0miya <i.tkomiya@gmail.com>
2011-05-20 20:27:07 +0900

    * actdiag_fontpath allows array of fontpaths

--- actdiag/setup.py
+++ actdiag/setup.py
@@ -32,7 +32,7 @@ requires = ['actdiag>=0.1.5', 'Sphinx>=0.6']
 
 setup(
     name='sphinxcontrib-actdiag',
-    version='0.1.0',
+    version='0.1.1',
     url='http://bitbucket.org/birkenfeld/sphinx-contrib',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-actdiag',
     license='BSD',
