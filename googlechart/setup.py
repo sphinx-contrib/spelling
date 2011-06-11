0f900537252d420460a14d3c33f40e0eb0a9bab9
tk0miya <i.tkomiya@gmail.com>
2011-06-11 12:42:39 +0900

    * Update version

--- googlechart/setup.py
+++ googlechart/setup.py
@@ -27,7 +27,7 @@ requires = ['Sphinx>=0.6', 'funcparserlib']
 
 setup(
     name='sphinxcontrib-googlechart',
-    version='0.1.0',
+    version='0.1.1',
     url='http://bitbucket.org/birkenfeld/sphinx-contrib',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-googlechart',
     license='BSD',
