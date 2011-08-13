9cce98360cfb44771b6f4f802082083c87c2748b
Doug Hellmann <doug.hellmann@gmail.com>
2011-08-13 19:40:52 -0400

    update version number in documentation

--- spelling/docs/source/conf.py
+++ spelling/docs/source/conf.py
@@ -51,7 +51,7 @@ copyright = u'2011, Doug Hellmann'
 # built documents.
 #
 # The short X.Y version.
-version = '1.1.1'
+version = '1.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
