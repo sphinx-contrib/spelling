b3454320c7593ef6a6739031aaf97a4cf90391c5
Sebastian Wiesner <lunaryorn@googlemail.com>
2011-08-25 10:32:19 +0200

    tip is issuetracker 0.9 now

--- issuetracker/sphinxcontrib/issuetracker.py
+++ issuetracker/sphinxcontrib/issuetracker.py
@@ -40,7 +40,7 @@ from __future__ import (print_function, division, unicode_literals,
                         absolute_import)
 
 
-__version__ = '0.8'
+__version__ = '0.9'
 
 import re
 import urllib2
