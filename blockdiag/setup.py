da67aee9acbe875888390f6783d10292faf88ca2
tk0miya <i.tkomiya@gmail.com>
2011-11-06 11:17:39 +0900

    Generate images forcely with unknown writers

--- blockdiag/setup.py
+++ blockdiag/setup.py
@@ -22,11 +22,11 @@ Following code is sample::
 This module needs blockdiag_.
 '''
 
-requires = ['blockdiag>=0.9.3', 'Sphinx>=0.6']
+requires = ['blockdiag>=1.0.0', 'Sphinx>=0.6']
 
 setup(
     name='sphinxcontrib-blockdiag',
-    version='0.9.3',
+    version='1.0.0',
     url='http://bitbucket.org/birkenfeld/sphinx-contrib',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-blockdiag',
     license='BSD',
