096590e6e4f0506c4735b5b5644bd8ad42e0a920
tk0miya <i.tkomiya@gmail.com>
2012-04-13 14:36:44 +0900

    Bump version

--- actdiag/setup.py
+++ actdiag/setup.py
@@ -32,7 +32,7 @@ requires = ['actdiag>=0.3.0', 'Sphinx>=0.6']
 
 setup(
     name='sphinxcontrib-actdiag',
-    version='0.3.2',
+    version='0.4.0',
     url='http://bitbucket.org/birkenfeld/sphinx-contrib',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-actdiag',
     license='BSD',
