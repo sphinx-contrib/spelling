e95f2c986f407e0c6f65ef1cd37bdefa98a01213
Stephen Sugden <glurgle@gmail.com>
2012-04-21 15:04:24 -0700

    Bump version for requirejs support
    --HG--
    extra : rebase_source : cf5a3545b3e1e2e70a8fe0607461564432a55464

--- coffeedomain/setup.py
+++ coffeedomain/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 if __name__ == '__main__':
     setup(name='sphinxcontrib-coffee',
-          version='0.1.0',
+          version='0.1.1',
           license='BSD',
           author="Stephen Sugden",
           author_email="glurgle@gmail.com",
