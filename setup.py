a2fcb99d6dcb9d7c5687dc94b6dba7b23842f6b9
Doug Hellmann <doug.hellmann@gmail.com>
2012-07-20 16:17:12 -0400

    move spelling files up to top of repo; fix URL in setup.py
    --HG--
    rename : spelling/LICENSE => LICENSE
    rename : spelling/MANIFEST.in => MANIFEST.in
    rename : spelling/Makefile => Makefile
    rename : spelling/README => README
    rename : spelling/announce.rst => announce.rst
    rename : spelling/docs/Makefile => docs/Makefile
    rename : spelling/docs/source/conf.py => docs/source/conf.py
    rename : spelling/docs/source/customize.rst => docs/source/customize.rst
    rename : spelling/docs/source/developers.rst => docs/source/developers.rst
    rename : spelling/docs/source/history.rst => docs/source/history.rst
    rename : spelling/docs/source/index.rst => docs/source/index.rst
    rename : spelling/docs/source/install.rst => docs/source/install.rst
    rename : spelling/docs/source/run.rst => docs/source/run.rst
    rename : spelling/docs/source/spelling_wordlist.txt => docs/source/spelling_wordlist.txt
    rename : spelling/nosetest.cfg => nosetest.cfg
    rename : spelling/setup.cfg => setup.cfg
    rename : spelling/setup.py => setup.py
    rename : spelling/sphinxcontrib/__init__.py => sphinxcontrib/__init__.py
    rename : spelling/sphinxcontrib/spelling.py => sphinxcontrib/spelling.py
    rename : spelling/test/test_builder.py => test/test_builder.py
    rename : spelling/test/test_checker.py => test/test_checker.py
    rename : spelling/test/test_wordlist.txt => test/test_wordlist.txt
    rename : spelling/tox.ini => tox.ini

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = ['Sphinx>=0.6',
 setup(
     name='sphinxcontrib-spelling',
     version='1.3',
-    url='http://bitbucket.org/birkenfeld/sphinx-contrib',
+    url='http://bitbucket.org/dhellmann/sphinxcontrib-spelling',
     license='BSD',
     author='Doug Hellmann',
     author_email='doug.hellmann@gmail.com',
