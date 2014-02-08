515f38304a29f9b2235919fbf74f26532d81d139
Doug Hellmann <doug@doughellmann.com>
2014-02-08 09:35:18 -0500

    add test for acronym filter

--- sphinxcontrib/spelling/tests/test_filter.py
+++ sphinxcontrib/spelling/tests/test_filter.py
@@ -6,14 +6,32 @@
 """Tests for filters.
 """
 
+from enchant.tokenize import get_tokenizer
+
 from sphinxcontrib.spelling import filters
 
 
-def test_unicode_builtin():
+def test_builtin_unicode():
     f = filters.PythonBuiltinsFilter(None)
     assert not f._skip(u'passé')
 
 
-def test_regular_builtin():
+def test_builtin_regular():
     f = filters.PythonBuiltinsFilter(None)
     assert f._skip('print')
+
+
+def test_acronym():
+    text = 'a front-end for DBM-style databases'
+    t = get_tokenizer('en_US', [])
+    f = filters.AcronymFilter(t)
+    words = [w[0] for w in f(text)]
+    assert 'DBM' not in words, 'Failed to filter out acronym'
+
+
+def test_acronym_unicode():
+    text = u'a front-end for DBM-style databases'
+    t = get_tokenizer('en_US', [])
+    f = filters.AcronymFilter(t)
+    words = [w[0] for w in f(text)]
+    assert u'DBM' not in words, 'Failed to filter out acronym'
