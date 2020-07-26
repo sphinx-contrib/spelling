7b04ee3b5998294e44b7b0e8fe8a3cbb4d114474
Doug Hellmann <doug@doughellmann.com>
2020-07-26 16:46:49 -0400

    clarify logic in sphinxcontrib/spelling/asset.py
    Sometimes the environment hasn't been initialized or doesn't have data related to input documents.
    
    Co-authored-by: Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>

--- sphinxcontrib/spelling/asset.py
+++ sphinxcontrib/spelling/asset.py
@@ -5,6 +5,7 @@
 """Asset collector for additional spelling terms."""
 
 import collections
+import contextlib
 
 from sphinx.environment.collectors import EnvironmentCollector
 from sphinx.util import logging
@@ -15,11 +16,15 @@ logger = logging.getLogger(__name__)
 class SpellingCollector(EnvironmentCollector):
 
     def clear_doc(self, app, env, docname) -> None:
-        if docname in getattr(env, 'spelling_document_words', {}):
+        with contextlib.suppress(AttributeError, KeyError):
             del env.spelling_document_words[docname]
 
     def merge_other(self, app, env, docnames, other):
-        other_words = getattr(other, 'spelling_document_words', {})
+        try:
+            other_words = other.spelling_document_words
+        except AttributeError:
+            other_words = {}
+
         if not hasattr(env, 'spelling_document_words'):
             env.spelling_document_words = collections.defaultdict(list)
         env.spelling_document_words.update(other_words)
