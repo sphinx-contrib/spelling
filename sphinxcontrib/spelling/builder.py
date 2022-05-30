325fabd0e3f5ac1c3fdf8b65a25e097051946b9e
Graham Smith <grhm.smith@gmail.com>
2022-05-30 16:07:58 +0100

    Split string into list iff contains commas

--- sphinxcontrib/spelling/builder.py
+++ sphinxcontrib/spelling/builder.py
@@ -106,12 +106,10 @@ class SpellingBuilder(Builder):
         if word_list is None:
             word_list = 'spelling_wordlist.txt'
 
-        if (isinstance(word_list, str) and 
-            word_list.startswith('[') and 
-            word_list.endswith(']')):
+        if isinstance(word_list, str) and word_list.find(',') > 0:
             # Wordlist is a list, formatted as a str.
             # Split it back into a list
-            word_list = word_list[1:-1].split(',')
+            word_list = word_list.split(',')
 
         if not isinstance(word_list, list):
             return os.path.join(self.srcdir, word_list)
