fe11679c1f92052f523662c18482a7acd4f04902
Doug Hellmann <doug@doughellmann.com>
2024-12-19 10:54:03 -0500

    fix history checker to look for main not master

--- tools/history-update.sh
+++ tools/history-update.sh
@@ -9,7 +9,7 @@ git branch -a
 # We only look at the files that have changed in the current PR, to
 # avoid problems when the template is changed in a way that is
 # incompatible with existing documents.
-if git log --name-only --pretty= "origin/master.." -- \
+if git log --name-only --pretty= "origin/main.." -- \
         | grep -q '^docs/source/history.rst$'; then
     echo "Found a change to history file."
     exit 0
